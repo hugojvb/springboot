3f447656be2d49395ee77cd5075168398e6ae9f0
Hugo Batista <hugojvb20@gmail.com>
2020-11-02 22:54:01 +0000

    Creating books model

---
+++ src/main/java/com/hugojvb/model/Books.java
@@ -0,0 +1,15 @@
+package com.hugojvb.model;  
+import javax.persistence.Column;  
+import javax.persistence.Entity;  
+import javax.persistence.Id;  
+import javax.persistence.Table;  
+
+@Entity
+@Table
+
+public class Books {
+
+    private int bookId;
+}
+
+
