8ade3a90493ce67657e71b3776243c46f5635659
Hugo Batista <hugojvb20@gmail.com>
2020-11-03 13:55:57 +0000

    added controller, finished books model

--- src/main/java/com/hugojvb/model/Books.java
+++ src/main/java/com/hugojvb/model/Books.java
@@ -8,8 +8,50 @@ import javax.persistence.Table;
 @Table
 
 public class Books {
-
+    @Id
+    @Column
     private int bookId;
+
+    @Column
+    private String bookName;
+
+    @Column
+    private String author;
+
+    @Column
+    private int price;
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
 
 
