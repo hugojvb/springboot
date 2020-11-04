34c06a61b026c7ea35855673a10c7533d44ee17a
Hugo Batista <hugojvb20@gmail.com>
2020-11-04 15:11:18 +0000

    wrap-up

--- src/main/java/com/hugojvb/controller/BooksController.java
+++ src/main/java/com/hugojvb/controller/BooksController.java
@@ -11,39 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import com.hugojvb.model.Books;  
-// import com.hugojvb.service.BooksService;  
+ 
 import com.hugojvb.service.BooksService;
 
 @RestController
 public class BooksController {
     @Autowired  
-    BooksService booksService;  
-    //creating a get mapping that retrieves all the books detail from the database   
+    BooksService booksService;    
     @GetMapping("/book")  
     private List<Books> getAllBooks()   
     {  
     return booksService.getAllBooks();  
     }  
-    //creating a get mapping that retrieves the detail of a specific book  
     @GetMapping("/book/{bookid}")  
     private Books getBooks(@PathVariable("bookid") int bookid)   
     {  
     return booksService.getBooksById(bookid);  
     }  
-    //creating a delete mapping that deletes a specified book  
     @DeleteMapping("/book/{bookid}")  
     private void deleteBook(@PathVariable("bookid") int bookid)   
     {  
     booksService.delete(bookid);  
     }  
-    //creating post mapping that post the book detail in the database  
     @PostMapping("/books")  
     private int saveBook(@RequestBody Books books)   
     {  
     booksService.saveOrUpdate(books);  
     return books.getBookId();  
     }  
-    //creating put mapping that updates the book detail   
     @PutMapping("/books")  
     private Books update(@RequestBody Books books)   
     {  
